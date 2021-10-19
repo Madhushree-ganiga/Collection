9be12f93f3fa874476030ba20bccc80d7164d9bf
Madhushree Ganiga/Javacode-Wolken <92204525+Madhushree-ganiga@users.noreply.github.com>
2021-10-19 08:47:51 +0530

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,23 @@
+mport java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Tester {
+
+	public static void main(String[] args) {
+	
+      System.out.println("hhhhhhh");
+      ArrayList arrayList=new ArrayList();
+      arrayList.add("hello");
+      arrayList.add(2344l);
+      
+      System.out.println(arrayList.get(1));
+      
+      for (Object object : arrayList) {
+    	  System.out.println(object);
+	}
+      System.out.println("end");
+      
+      
+	}
+
+}
